0e30c79fbd4583eb222d86acdbe63510db025b8a
SandibelS <17-10614@usb.ve>
2026-01-30 23:55:59 -0400

    Add new vars

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -12,3 +12,25 @@ func _process(delta: float) -> void:
 	
 var name_player = "Simon Bolivar"
 var tuqueque_free = false
+
+########### VARIABLES PARA LA LOGICA DE LOS DIALOGOS ##############
+
+var are_dialogues_on = false
+var dialogues = {
+	"scene1": {
+		"boy1": [
+			"Al fin logré recolectar un millón de oro en RuneScape!", 
+			"Aja, como era la vaina para vender esto...  ",
+			"Por qué el internet anda tan lento?",
+			"NOOOOOOOOOOOOOOOOOOOOOOOOO",
+			"Cant'tv se volvio a ir"
+		]
+			
+			
+	},
+	
+	"scene2": {
+		
+	},
+	
+}
