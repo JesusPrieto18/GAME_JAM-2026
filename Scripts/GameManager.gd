ad04b2db76d459bff6eddba422d69c3950f9204e
SandibelS <17-10614@usb.ve>
2026-01-31 14:01:42 -0400

    Add new logic vars

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -1,35 +1,117 @@
 extends Node
 
-
+################ FUNCIONES INICIALES ###################
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-	
+
+#########################################################
+
+### DATOS DEL JUGADOR ###
 var name_player = "El niñe"
+
+### LOGICA DE LA HISTORIA ####
 var tuqueque_free = false
+
+### LOGICA DE PUZZLES ########
+
+# Puzzle1: Distraer a mamá
+var puzzle1 = {
+				"resolved": false,
+				"help_dialog": "Distrae a tu mamá para salvar al tuqueque"
+}
+
+# Puzzle2: Arepa
+var puzzle2 = {
+				"resolved": false,
+				"help_dialog": "Encuentra los ingredientes de la arepa para la rata"
+}
+
+# Puzzle3: Mango
+var puzzle3 = {
+				"resolved": false,
+				"help_dialog": "Mueve las piedras",
+}
+
+# Puzzle4: Forum
+var puzzle4 = {
+				"resolved": false,
+				"help_dialog": "Evita los trabajadores y escapa"
+}
 
 ########### VARIABLES PARA LA LOGICA DE LOS DIALOGOS ##############
 
 var are_dialogues_on = false
 var dialogues = {
-	"scene1": {
+	
+	##### EL NiÑO PIERDE EL INTERNET Y LA MAMA LO LLAMA
+	0: [
+			{
 				"name": name_player,
 				"text": [
 					"Al fin logré recolectar un millón de oro en RuneScape!", 
 					"Aja, como era la vaina para vender esto...  ",
 					"Por qué el internet anda tan lento?",
-					"NOOOOOOOOOOOOOOOOOOOOOOOOO!!!!",
-					"Cant'tv se volvio a ir..."
+					"NOOOOOOOOOOOOOOOOOOOOOOOOO",
+					"Cant'tv se volvio a ir...",
+					"TT_TT"
+					
+				]
+			},
+			
+			{
+				"name": "Mamá",
+				"text": [
+					"¡{name_player}! Ven a ayudarme con el almuerzo", 
+				]
+			},	
+			
+			{
+				"name": name_player,
+				"text": [
+					"VOOOOOOOY", 
 				]	
-	},
+			}
+	],
 	
-	"scene2": {
+	##### EL NIÑO ENTRA LA COCINA Y ESCUCHA AL TUQUEQUE ATERRORIZADO
+	1: [
+			{
+				"name": "Tuqueque",
+				"text": [
+					"AUXILIOOOOOOOOOOOOOOOOOOOOOOOOOOO", 
+					"NO SOY COMIDA SEÑORAAAAAAAAA"
+				]	
+			},
+			
+			{
+				"name": "Mamá",
+				"text": [
+					"Que buena torta de morrocoy voy a hacer!", 
+				]
+			},
+			
+			{
+				"name": "Tuqueque",
+				"text": [
+					"NO SOY UN MORROCOY SEÑORA POR EL AMOR AL SANTO", 
+					"EH!! TÚ!! CARAJITO!! AYUDAMEEEEE",
+					"TE PROMETO QUE TE COMPENSARE!!"
+				]	
+			},
+			
+			{
+				"name": name_player,
+				"text": [
+					"Las cosas que trae el KLAP cada vez son más raras...",
+					"Creo que si distraigo a mamá puedo salavar a ese amiguin" 
+				]	
+			}
 		
-	},
+	],
 	
 }
